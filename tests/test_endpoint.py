83bf1bd817acd5f95fb12ad5fb567c5c9b29a010
Narmatov Dinnur <runniddefault@gmail.com>
2024-11-19 23:29:35 +0600

    updated endpoint tests

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,25 +1,80 @@
 import pytest
-import pytest_asyncio
-from httpx import AsyncClient, ASGITransport
+from fastapi.testclient import TestClient
 
 from main import app
 
-
-@pytest_asyncio.fixture(scope="function")
-async def api_client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as async_cli:
-        yield async_cli
+client = TestClient(app)
 
 
-@pytest.mark.asyncio
-async def test_success_calculation_request(api_client: AsyncClient):
-    response = await api_client.post(
-        "/api/calculate-discount/",
-        json={
-            "amount": "150.00",
-            "is_loyal": True
-        }
-    )
+@pytest.mark.parametrize(
+    "payload, expected_response",
+    [
+        # No quantity discount
+        (
+            # input
+            {
+                "amount": "50.00",
+                "is_loyal": False,
+                "cart_items": [
+                    {"id": 1, "quantity": 1, "price": "50.00"}
+                ],
+            },
+            # expected output
+            {
+                "original_amount": "50.00",
+                "quantity_discount": "0.00",
+                "loyalty_discount": "0.00",
+                "final_amount": "50.00",
+                "applied_discounts": [],
+            },
+        ),
+        # With quantity discount
+        (
+            # input
+            {
+                "amount": "150.00",
+                "is_loyal": False,
+                "cart_items": [
+                    {"id": 1, "quantity": 2, "price": "50.00"},
+                    {"id": 2, "quantity": 1, "price": "50.00"}
+                ],
+            },
+            # expected output
+            {
+                "original_amount": "150.00",
+                "quantity_discount": "15.00",
+                "loyalty_discount": "0.00",
+                "final_amount": "135.00",
+                "applied_discounts": ["quantity"],
+            },
+        ),
+        # With quantity and loyalty discounts
+        (
+            # input
+            {
+                "amount": "150.00",
+                "is_loyal": True,
+                "cart_items": [
+                    {"id": 1, "quantity": 2, "price": "50.00"},
+                    {"id": 2, "quantity": 1, "price": "50.00"}
+                ],
+            },
+            # expected output
+            {
+                "original_amount": "150.00",
+                "quantity_discount": "15.00",
+                "loyalty_discount": "6.75",
+                "final_amount": "128.25",
+                "applied_discounts": ["quantity", "loyalty"],
+            },
+        ),
+    ],
+)
+def test_calculate_discount(payload, expected_response):
+    response = client.post("/api/calculate-discount/", json=payload)
     assert response.status_code == 200, response.json()
 
+    print(response.json())
+    print(expected_response)
 
+    assert response.json() == expected_response, response.json()
